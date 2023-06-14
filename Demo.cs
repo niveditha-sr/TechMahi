9b5057803d548b05c9648278cc42a10445e52243
niveditha-sr <nivedithaneetha63@gmail.com>
2023-06-14 22:47:35 +0530

    First commit

---
+++ Demo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projectdevops
+{
+    internal class Demo
+    {
+        public string sayHello()
+        {
+            Console.WriteLine("Welcome");
+            Console.WriteLine("hello");
+            return "hi";
+        }
+    }
+}
